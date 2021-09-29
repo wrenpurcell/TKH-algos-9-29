d6e2d8a75a8f369571a8b74891a2b02c7d22efc4
Wren Purcell <purcell856@gmail.com>
2021-09-29 11:23:07 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+//write a function that adds 'ing' to an array of verbs and returns them as a new array
+function addIng(arr){
+//your code here
+}
+
+console.log(addIng(["eat", "sleep", "read"]);
+            
+/*
+write a function that takes in a string and turns it into pig latin ex cat = atcay 
+if the word starts with a vowel, you should add 'yay' to the end. example: apple = appleyay
+return the new pig latin string
+*/
+function pigLatin(str){
+//your code here
+}
+console.log(pigLatin("cat"));
+console.log(pigLatin("apple"));
+
+//write a function that counts the number of vowels in a string and return the number of vowels
+//hint look up .includes() method 
+const vowels = ["a", "e", "i", "o", "u"]
+function countVowels(text) {
+  //your code here
+}
+console.log(countVowels("banana"));
